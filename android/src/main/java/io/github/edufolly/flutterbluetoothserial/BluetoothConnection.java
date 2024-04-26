db49e8e94ac03328333da7fe6e7837bc7f34ce2b
LolbiKB <36408961+LolbiKB@users.noreply.github.com>
2024-04-26 13:22:25 -0400

    Update BluetoothConnection.java

--- android/src/main/java/io/github/edufolly/flutterbluetoothserial/BluetoothConnection.java
+++ android/src/main/java/io/github/edufolly/flutterbluetoothserial/BluetoothConnection.java
@@ -62,7 +62,7 @@ public abstract class BluetoothConnection
     }
     /// Connects to given device by hardware address (default UUID used)
     public void connect(String address) throws IOException {
-        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         BluetoothDevice device = btAdapter.getRemoteDevice(address);
         ParcelUuid[] uuids = (ParcelUuid[]) device.getUuids();
         connect(address, uuids[0].getUuid());
